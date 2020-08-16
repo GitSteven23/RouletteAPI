9445c936fe5f5996127710e9dda6d54a55266100
Steven Chacón <steven2010_13@hotmail.com>
2020-08-16 17:03:06 -0500

    Commit - Creacion del proyecto e integración con GitHub

---
+++ RouletteAPI/RouletteAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace RouletteAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ RouletteAPI/RouletteAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace RouletteAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
